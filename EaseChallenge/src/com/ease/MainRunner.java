7c4b99b40fa19787d364bba00a1fe76b4a55a6a5
Shufreezy <palacioalbertdale@gmail.com>
2018-06-09 17:16:09 +0800

    Added main runner
    Results:
    	(Steepest)
    	Highest Drop Path: 1499-0
    	Highest Drop Value: 1499
    	Highest Drop Length: 2
    
    	Longest Drop Path: 1422-1412-1316-1304-1207-1162-965-945-734-429-332-310-214-143-0
    	Longest Drop Value: 1422
    	Longest Drop Length: 15

--- EaseChallenge/src/com/ease/MainRunner.java
+++ EaseChallenge/src/com/ease/MainRunner.java
@@ -1,9 +1,27 @@
 package com.ease;
 
+import java.io.File;
+import java.util.Map;
+
+import com.ease.algorithm.Search;
+import com.ease.algorithm.model.SearchModel;
+import com.ease.filehandler.FileHandler;
+import com.ease.filehandler.dto.FileHandlerDto;
+
 public class MainRunner {
 
 	public static void main(String[] args) {
-
+		FileHandler fh = new FileHandler();
+		FileHandlerDto dto = new FileHandlerDto();
+		dto = fh.readFile(new File("").getAbsolutePath() + File.separator + "src" + File.separator + "com" + File.separator + "ease" + File.separator + "resources" + File.separator + "map.txt");
+		Search search = new Search();
+		Map<String, SearchModel> returnVal = search.processGrid(dto);
+		System.out.println("Highest Drop Path: " + returnVal.get("highestDrop").getPath());
+		System.out.println("Highest Drop Value: " + returnVal.get("highestDrop").getDrop());
+		System.out.println("Highest Drop Length: " + returnVal.get("highestDrop").getLength());
+		System.out.println("Longest Drop Path: " + returnVal.get("longestDrop").getPath());
+		System.out.println("Longest Drop Value: " + returnVal.get("longestDrop").getDrop());
+		System.out.println("Longest Drop Length: " + returnVal.get("longestDrop").getLength());
 	}
 
 }
